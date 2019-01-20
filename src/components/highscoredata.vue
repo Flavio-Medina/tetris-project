55835134fe483a7c50e124cc660b6abc880f868e
flavio-medina <flavio.medina@hotmail.de>
2019-01-20 21:05:58 +0100

    Highscore-List

--- src/components/highscoredata.vue
+++ src/components/highscoredata.vue
@@ -1,86 +1,43 @@
 <template>
-  <div>
-    <b-table
-      :sort-by.sync="sortBy"
-      :sort-desc.sync="sortDesc"
-      :items="items"
-      :fields="fields"
-      ref="hst"
-      class="bt"
-    >
-      <template slot="index" slot-scope="data">
-        <div id="ind">{{data.index + 1}}</div>
-      </template>
-      <template slot="user" slot-scope="data">
-        <div id="username">{{data.item.user}}</div>
-      </template>
-      <template slot="lines" slot-scope="data">
-        <div>{{data.item.lines}}</div>
-      </template>
-      <template slot="score" slot-scope="data">
-        <div>{{data.item.score}}</div>
-      </template>
-    </b-table>
-    <b-btn class="col align-self-center btn btn-primary smbtn" v-on:click="refresht">Refresh</b-btn>
-  </div>
+  <div clas="container">
+      <table class="table table-striped table-borderes">
+      <thead>
+        <tr>
+          <th class="center">Username</th>
+          <th class="center">Lines</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in User" :key="item._id">
+          <td class="text-left"> {{ item.username }}</td>
+          <td class="text-left"> {{ item.lines }}</td>
+          <td class="text-left"> {{ item.score }}</td>
+        </tr>
+      </tbody>
+    </table>
+      </div>
 </template>
 
 <script>
-let userd = [];
-let scored = [];
-let linesd = [];
-let items;
-let fields;
-
 import axios from 'axios';
 export default {
-  data() {
-    return {
-      sortBy: "score",
-      sortDesc: true,
-      items: items,
-      user: userd,
-      score: scored,
-      lines: linesd,
-      fields: [
-        { key: "index", label: "" },
-        "user",
-        { key: "lines", sortable: true },
-        { key: "score", sortable: true }
-      ],
-      High: []
-    };
-  },
-  methods: {
-    refresht: function() {
-      beforeCreate();
-    }
-  },
-  beforeCreate: function() {
-    
-//     userd[0] = "test1";
-//     scored[0] = "2";
-//     linesd[0] = "4";
-//     userd[1] = "test2";
-//     scored[1] = "3";
-//     linesd[1] = "3";
-//     items = [
-//       { user: userd[0], score: scored[0], lines: linesd[0] },
-//       { user: userd[1], score: scored[1], lines: linesd[1] },
-//       { user: userd[2], score: scored[2], lines: linesd[2] },
-//       { user: userd[3], score: scored[3], lines: linesd[3] }
-//     ];
-//     console.log("Test");
-    axios.get("/user/secureroute")
-      .then((response) => {
-        console.log(response.data);
-        this.High = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-};
+    name:'user',
+    data() {
+        return {
+            User: [],
+        }
+    },
+        mounted() {
+        axios.get('/user/secureroute')
+            .then((response) => {
+                this.User = response.data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+}
 </script>
 
 <style scoped>
